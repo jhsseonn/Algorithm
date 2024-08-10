f146fca998c5c2fbd20a0d132b51335fa3637f97
jhsseonn <hs125bt@gmail.com>
2024-08-10 23:46:12 +0900

    240810 백준 9095 풀이

---
+++ src/BOJ/recursive/S3_9095_123더하기.java
@@ -0,0 +1,38 @@
+package BOJ.recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_9095_123더하기 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int sum = 0;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            getSum(N);
+            System.out.println(count);
+            count = 0;
+        }
+    }
+
+    static void getSum(int N) {
+        if (sum == N) {
+            count++;
+            return;
+        } else if (sum > N) return;
+
+        getSum(N - 1);
+        getSum(N - 2);
+        getSum(N - 3);
+    }
+}
