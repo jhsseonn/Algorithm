fc1eb39006ee4b8ea3ac3d5289575a2719a4a17e
jhsseonn <hs125bt@gmail.com>
2024-08-23 00:47:59 +0900

    240822 swea 보호필름

--- src/swea/모의SW/Solution_2112_모의SW_보호_필름_장효선.java
+++ src/swea/모의SW/Solution_2112_모의SW_보호_필름_장효선.java
@@ -12,10 +12,9 @@ public class Solution_2112_모의SW_보호_필름_장효선 {
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static int result;
-	static int D, W, K, filmNum;
-	static int[] subSetIdx, isChecked;
+	static int D, W, K;
+	static int[] isChecked;
 	static int[][] board, boardCopy;
-	static boolean isValid;
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
@@ -29,9 +28,7 @@ public class Solution_2112_모의SW_보호_필름_장효선 {
 			K = Integer.parseInt(st.nextToken());
 			board = new int[D][W];
 			boardCopy = new int[D][W];
-			subSetIdx = new int[D];
 			isChecked = new int[D];
-			isValid = false;
 			
 			for (int i = 0; i < D; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -40,24 +37,23 @@ public class Solution_2112_모의SW_보호_필름_장효선 {
 				}
 				boardCopy[i] = board[i].clone();
 			}
-			
+
 			Arrays.fill(isChecked, -1);
 			getSubset(0, 0);
-			
+
 			sb.append("#").append(test_case).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
 
 	private static void getSubset(int depth, int len) {
-		if (len>=result) return;
 		if (depth==D) {
-//			System.out.println(Arrays.toString(subset));
-			for (int i = 0; i < isChecked.length; i++) {
+			if (len>=result) return;
+			for (int i = 0; i < D; i++) {
+				boardCopy[i] = board[i].clone();
 				if (isChecked[i]!=-1) changeFilm(i, isChecked[i]);
 			}
-			if (checkFilm()) result = len;
-//			dfs(0);
+			if (checkFilm()) result = Math.min(result, len);
 			return;
 		}
 		
@@ -81,17 +77,26 @@ public class Solution_2112_모의SW_보호_필름_장효선 {
 	}
 	
 	private static boolean checkFilm() {
-		int cnt = 1;
+		int cnt, now;
 		for (int i = 0; i < W; i++) {
 			boolean pass = false;
-			
-			for (int j = 0; j < D-1; j++) {
-				if (boardCopy[j][i]==boardCopy[j+1][i]) cnt+=1;
-				else cnt=1;
+			now = boardCopy[0][i];
+			cnt = 1;
+
+			for (int j = 1; j < D; j++) {
+				if (boardCopy[j][i]==now) {
+					cnt+=1;
+				} else {
+					now = boardCopy[j][i];
+					cnt=1;
+				}
+				if (cnt>=K) {
+					pass=true;
+					break;
+				}
 			}
-			if (cnt>=K) pass=true;
-			if (pass==true) continue;
-			else return false;
+
+			if (!pass) return false;
 		}
 		return true;
 	}
