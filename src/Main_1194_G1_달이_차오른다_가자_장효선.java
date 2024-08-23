867cbc9325f4165543dd68617c62b88f03e20c68
jhsseonn <hosu0125@naver.com>
2024-08-23 17:54:39 +0900

    0823 아카이빙

---
+++ src/Main_1194_G1_달이_차오른다_가자_장효선.java
@@ -0,0 +1,111 @@
+package com.ssafy.ws;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_1194_G1_달이_차오른다_가자_장효선 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N, M, startR, startC;
+	static char[][] map;
+	static boolean[][][] visited;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static List<Character> keys = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'));
+	static List<Character> doors = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F'));
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new char[N][M];
+		visited = new boolean[N][M][2<<6];
+		
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = input.charAt(j);
+				// 민식이가 서 있는 위치를 bfs 시작 위치로 한다
+				if (map[i][j]=='0') {
+					startR = i;
+					startC = j;
+				}
+			}
+		}
+		
+		System.out.println(bfs());
+	}
+	
+	private static int bfs() {
+		Deque<Maze> q = new ArrayDeque<>();
+		q.offer(new Maze(startR, startC, 0, 0));
+		visited[startR][startC][0] = true;
+		
+		while(!q.isEmpty()) {
+			Maze m = q.poll();
+			int r = m.r;
+			int c = m.c;
+			int key = m.key;
+			int distance = m.distance;
+			
+			// 출구면 탈출한다
+			if (map[r][c]=='1') {
+				return distance;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nr = r+dr[i];
+				int nc = c+dc[i];
+				
+				if (nr<=-1 || nr>=N || nc<=-1 || nc>=M || map[nr][nc]=='#') {
+					continue;
+				}
+				
+				// A, B, C, D, E, F이면 현재 열쇠 정보를 확인한다(비트마스킹)
+				if (doors.contains(map[nr][nc])) {
+					if ((key & (1<<map[nr][nc]-'A'))==0) {
+						continue;
+					}
+				}
+				
+				// a, b, c, d, e, f 중 하나면 열쇠 정보를 받아준다
+				int nk = keys.contains(map[nr][nc]) ? key | 1<<(map[nr][nc]-'a') : key;
+				
+
+				if (visited[nr][nc][nk]) {
+					continue;
+				}
+				
+				// . 이면 이동한다
+				visited[nr][nc][nk] = true;
+				q.offer(new Maze(nr, nc, key, distance+1));
+			}
+		}
+		
+		return -1;
+	}
+	
+	private static class Maze {
+		int r;
+		int c;
+		int key;
+		int distance;
+		
+		public Maze(int r, int c, int key, int distance) {
+			this.r = r;
+			this.c = c;
+			this.key = key;
+			this.distance = distance;
+		}
+	}
+
+}
---
+++ src/Main_1600_G3_말이_되고픈_원숭이_장효선.java
@@ -0,0 +1,107 @@
+package com.ssafy.ws;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main_1600_G3_말이_되고픈_원숭이_장효선 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int[][] travel;
+	static boolean[][][] visited;
+	static int W, H, K;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int[] hdr = { -2, -2, -1, -1, 2, 2, 1, 1 };
+	static int[] hdc = { -1, 1, -2, 2, -1, 1, -2, 2 };
+
+	public static void main(String[] args) throws IOException {
+		K = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		travel = new int [H][W];
+		visited = new boolean [H][W][K+1];
+
+		for (int i = 0; i < H; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < W; j++) {
+				travel[i][j] = Integer.parseInt(st.nextToken());
+				for (int k = 0; k < K+1; k++) {
+					visited[i][j][k] = false;
+				}
+			}
+		}
+
+		System.out.println(bfs());
+	}
+
+	private static int bfs() {
+		Deque<Map> q = new ArrayDeque<>();
+
+		// 시작 노드 넣고 방문처리
+		q.offer(new Map(0, 0, 0, 0));
+		visited[0][0][0] = true;
+
+		while (!q.isEmpty()) {
+			Map node = q.poll();
+			int r = node.r;
+			int c = node.c;
+			int move = node.move;
+			int horseMove = node.horseMove;
+			
+			if (r==H-1 && c==W-1) {
+				return move+horseMove;
+			}
+			
+			// k<K인 경우 말의 이동으로 이동 시도
+			// 아닌 경우 무조건 원숭이 이동
+			if (horseMove < K) {
+				for (int i = 0; i < 8; i++) {
+					int hnr = r + hdr[i];
+					int hnc = c + hdc[i];
+
+					if (hnr > -1 && hnr < H && hnc > -1 && hnc < W && travel[hnr][hnc] != 1 && !visited[hnr][hnc][horseMove+1]) {
+						travel[hnr][hnc] = 1;
+						visited[hnr][hnc][horseMove+1] = true;
+						q.offer(new Map(hnr, hnc, move, horseMove+1));
+					}
+				}
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+
+				if (nr > -1 && nr < H && nc > -1 && nc < W && travel[nr][nc] != 1 && !visited[nr][nc][horseMove]) {
+					travel[nr][nc] = 1;
+					visited[nr][nc][horseMove] = true;
+					q.offer(new Map(nr, nc, move+1, horseMove));
+				}
+			}
+			
+		}
+		
+		return -1;
+	}
+
+	private static class Map {
+		int r;
+		int c;
+		int move;
+		int horseMove;
+
+		public Map(int r, int c, int move, int horseMove) {
+			this.r = r;
+			this.c = c;
+			this.move = move;
+			this.horseMove = horseMove;
+		}
+	}
+
+}
