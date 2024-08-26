6ea60ab0271841987808d297622be76f81f3c317
jhsseonn <hs125bt@gmail.com>
2024-08-26 22:26:35 +0900

    240826 BOJ 17070 파이프 옮기기1

--- src/BOJ/graph/Main_17070_G5_파이프_옮기기1_장효선.java
+++ src/BOJ/graph/Main_17070_G5_파이프_옮기기1_장효선.java
@@ -1,10 +1,13 @@
-package com.ssafy.ws;
+package BOJ.graph;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 메모리: 168,264kb / 시간: 464ms
+ */
 public class Main_17070_G5_파이프_옮기기1_장효선 {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,15 +31,18 @@ public class Main_17070_G5_파이프_옮기기1_장효선 {
 			}
 		}
 		
-		dfs(0, 0);
+		dfs(new Pipe(0, 1, 0));
 		
 		System.out.println(ans);
 	}
 
-	private static void dfs(int r, int c) {
-		
+	private static void dfs(Pipe pipe) {
+		int r = pipe.r;
+		int c = pipe.c;
+		int state = pipe.state;
 		
 		if (r==N-1 && c==N-1) {
+//			System.out.println(r+" "+c+" "+state);
 			ans++;
 			return;
 		}
@@ -44,37 +50,41 @@ public class Main_17070_G5_파이프_옮기기1_장효선 {
 		for (int i = 0; i < 3; i++) {
 			int nr = r+dr[i];
 			int nc = c+dc[i];
-			
-			// 벽이거나 방문한 경우 건너뛰기
+
+			if (nr<0 || nr>=N || nc<0 || nc>=N) continue;
+
+			// 벽인 경우 건너뛰기
 			if (house[nr][nc]==1) continue;
-			if (visited[nr][nc]) continue;
-			
-			// 대각선으로 갈 경우 세 칸 모두 비어있어야함
+
+			// 파이프가 가로로 있었던 경우 아래로 갈 수 없음, 파이프가 세로로 있었던 경우 옆으로 갈 수 없음
+			// 파이프가 대각선으로 있는 경우 세칸 모두 비어 있어야만 이동할 수 있음
+			if (i==0 && state==2) continue;
+			if (i==2 && state==0) return;
 			if (i==1) {
-				
+				if (house[nr-1][nc]!=0 || house[nr][nc-1]!=0) continue;
 			}
-			
+
+			// 방문한 경우 건너뛰기
+			if (visited[nr][nc][i]) continue;
+
 			// 방문 처리
-			else if (house[nr][nc]==0 && !visited[nr][nc]) {
-				visited[nr][nc]=true;
-				dfs(nr, nc);
-				visited[nr][nc]=false;
+			if (house[nr][nc]==0 && !visited[nr][nc][i]) {
+				visited[nr][nc][i]=true;
+				dfs(new Pipe(nr, nc, i));
+				visited[nr][nc][i]=false;
 			}
 		}
-		
 	}
 	
-	private static class State {
+	private static class Pipe {
 		int r;
 		int c;
-		int br;
-		int bc;
+		int state;
 		
-		public State(int r, int c, int br, int bc) {
+		public Pipe(int r, int c, int state) {
 			this.r = r;
 			this.c = c;
-			this.br = br;
-			this.bc = bc;
+			this.state = state;
 		}
 	}
 }
