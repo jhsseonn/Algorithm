899243ec78dc47f4f10a7d1dfc089a2e4cbcf7f9
jhsseonn <hs125bt@gmail.com>
2024-08-28 01:38:11 +0900

    240827 BOJ 17143 낚시왕

--- src/BOJ/simulation/Main_17143_G1_낚시왕_장효선.java
+++ src/BOJ/simulation/Main_17143_G1_낚시왕_장효선.java
@@ -1,9 +1,11 @@
-package com.ssafy.ws;
+package BOJ.simulation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -13,15 +15,14 @@ public class Main_17143_G1_낚시왕_장효선 {
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static int R, C, M, ans;
-	static int[][] arr;
-	static List<Shark> sharks = new ArrayList<>();
+	static Shark[][] arr;
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new int[R][C];
+		arr = new Shark[R][C];
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -30,9 +31,8 @@ public class Main_17143_G1_낚시왕_장효선 {
 			int s = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			sharks.add(new Shark(r-1, c-1, s, d, z));
-			// 상어가 있는 칸은 상어의 크기로 초기화
-			arr[r-1][c-1] = z; 
+			// 상어가 있는 칸 초기화
+			arr[r-1][c-1] = new Shark(r-1, c-1, s, d, z);
 		}
 		
 		for (int i = 0; i < C; i++) {
@@ -46,78 +46,83 @@ public class Main_17143_G1_낚시왕_장효선 {
 	private static void fishing(int c) {
 		// 제일 가까운 상어를 잡음
 		for (int i = 0; i < R; i++) {
-			if (arr[i][c]!=0) {
-				System.out.println("round: "+c+" get: "+arr[i][c]);
-				ans+=arr[i][c];  // 상어 잡음
-				arr[i][c] = 0;  // 잡은 곳 초기화
-				removeShark(i, c);
+			if (arr[i][c]!=null) {
+				ans+=arr[i][c].z;  // 상어 잡음
+				arr[i][c] = null;  // 잡은 곳 초기화
 				break;
 			}
 		}
-		
+
 		// 상어들이 움직임
 		moveShark();
 	}
 	
-	private static void removeShark(int r, int c) {
-		for (int i = 0; i < sharks.size(); i++) {
-			if (sharks.get(i).r ==r && sharks.get(i).c == c) sharks.remove(i);
-		}
-	}
-	
 	private static void moveShark() {
-		// 상어 리스트를 돌면서
-		for (int i = 0; i<sharks.size(); i++) {
-			Shark sk = sharks.get(i);
+		Deque<Shark> dq = new ArrayDeque<>();
+		int[] dr = {-1, 1, 0, 0};
+		int[] dc = {0, 0, 1, -1};
+
+		// 상어들 큐에 추가
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (arr[i][j]!=null) {
+					dq.offer(new Shark(i, j, arr[i][j].s, arr[i][j].d, arr[i][j].z));
+				}
+			}
+		}
+
+		arr = new Shark[R][C];
+
+		while(!dq.isEmpty()) {
+			Shark sk = dq.poll();
 			int r = sk.r;
 			int c = sk.c;
 			int s = sk.s;
 			int d = sk.d;
-			
-			int nr = r;
-			int nc = c;
+			int z = sk.z;
+
 			// 상어의 위치와 방향을 계산한다
+			// 속력 최소화
+			// 상, 하
 			if (d==1 || d==2) {
-				s -= 2*(R-1);  // 속력의 나머지, 여기까지 방향은 그대로
-				if (d==2 && r+s >= R) {  // 배열의 범위를 초과한 경우
-					nr = 2*(R-1)-(r+s);
-					d = 1;
-				} else if (d==1 && r-s < 0) {
-					nr = s-r;
-					d = 2;
-				} 
+				s =s% (2*(R-1));
 			}
-			
+
+			// 우, 좌
 			if (d==3 || d==4) {
-				s -= 2*(C-1);  // 속력의 나머지, 여기까지 방향은 그대로
-				if (d==3 && c+s >= C) {  // 배열의 범위를 초과한 경우
-					nc = 2*(C-1)-(c+s);
-					d = 4;
-				} else if (d==4 && c-s < 0) {
-					nc = s-c;
-					d = 3;
-				}
+				s = s% (2*(C-1));
 			}
-			
-			// 해당 칸으로 상어 이동(기존 칸은 0으로 초기화)
-			arr[r][c] = 0;
-			// 상어의 위치 갱신
-			sk.r = nr;
-			sk.c = nc;
+
+			for (int i = 0; i < s; i++) {
+				int nr = r+dr[d-1];
+				int nc = c+dc[d-1];
+
+				if (nr<0 || nr>=R || nc<0 || nc >= C) { // 좌표가 범위 밖인 경우
+					r -= dr[d-1];
+					c -= dc[d-1];
+					if (d==1 || d==3) d+=1;
+					else d-=1;
+					continue;
+				}
+
+				// 위치 벗어나지 않은 경우 새로운 위치로 이동
+				r= nr;
+				c = nc;
+				d = d;
+			}
+
+			sk.r = r;
+			sk.c = c;
+			sk.d = d;
 			
 			// 만약 해당 칸에 이미 상어가 존재한다면, 크기가 더 큰 경우에만 갱신
-			if (arr[nr][nc]!=0) {
-				arr[nr][nc] = Math.max(arr[nr][nc], sk.z);
+			if (arr[r][c]!=null) {
+				if (arr[r][c].z < z) {
+					arr[r][c] = new Shark(r, c, s, d, z);
+				}
 			} else {
-				arr[nr][nc] = sk.z;
+				arr[r][c] = new Shark(r, c, s, d, z);
 			}
-			
-			// 크기 더 작으면 sharks에서 삭제(잡아먹힘)
-			if (arr[nr][nc]!=sk.z) {
-				sharks.remove(i);
-				i--;
-			}
-			
 		}
 	}
 
