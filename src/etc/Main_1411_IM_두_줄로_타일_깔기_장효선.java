b6ae61918a4085badd32f2fbc21dbbb0822a6ee2
jhsseonn <hs125bt@gmail.com>
2024-09-05 01:23:01 +0900

    240904 정올 1411 두줄로 타일 깔기

--- src/etc/Main_1411_IM_두_줄로_타일_깔기_장효선.java
+++ src/etc/Main_1411_IM_두_줄로_타일_깔기_장효선.java
@@ -1,4 +1,4 @@
-package com.ssafy.ws;
+package etc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,9 +15,15 @@ public class Main_1411_IM_두_줄로_타일_깔기_장효선 {
 	public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(br.readLine());
 		dp = new int[N+1];
-		
-		for (int i = 2; i < N+1; i++) {
-			dp[i] = dp[i-1]+2*dp[i-2];
+
+		if(N==1) System.out.println(1);
+		if(N==2) System.out.println(3);
+
+		dp[1] = 1;
+		dp[2] = 3;
+
+		for (int i = 3; i < N+1; i++) {
+			dp[i] = (dp[i-1]+2*dp[i-2])%20100529;
 		}
 		
 		System.out.println(dp[N]%20100529);
