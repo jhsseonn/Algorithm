a152fd9535504fd2733aa83588fb4bb32f1a8a73
janghyoseon <hosu0125@naver.com>
2024-09-09 23:38:57 +0900

    240909 문제풀이중

--- src/BOJ/graph/Main_11779_G3_최소비용_구하기_2.java
+++ src/BOJ/graph/Main_11779_G3_최소비용_구하기_2.java
@@ -3,9 +3,7 @@ package BOJ.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main_11779_G3_최소비용_구하기_2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,11 +11,20 @@ public class Main_11779_G3_최소비용_구하기_2 {
     static StringBuilder sb = new StringBuilder();
     static int N, M;
     static List<Node>[] adjList;
+    static List<Integer> path = new ArrayList<>();
+    static long[] minDistance;
+    static long INF = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
         adjList = new ArrayList[N+1];
+        minDistance = new long[N+1];
+
+        for (int i = 0; i < N+1; i++) {
+            adjList[i] = new ArrayList<>();
+            minDistance[i] = INF;
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -33,10 +40,37 @@ public class Main_11779_G3_최소비용_구하기_2 {
         int end = Integer.parseInt(st.nextToken());
 
         getMinDistance(start, end);
+
+        sb.append(minDistance[end]).append("\n");
+        sb.append(path.size()).append("\n");
+        for (int i = 0; i < path.size(); i++) {
+            sb.append(path.get(i)).append(" ");
+        }
+        System.out.println(sb);
     }
 
     private static void getMinDistance(int start, int end) {
+        minDistance[start] = 0;
+        boolean[] visited = new boolean[N+1];
+        path.add(start);
 
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Long.compare(a.w, b.w));
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (visited[cur.to]) continue;
+            if (minDistance[cur.to] < cur.w) continue;
+
+            for (Node e:adjList[cur.to]) {
+                if (visited[e.to]) continue;
+                if (minDistance[e.to] < cur.w+e.w) continue;
+                visited[e.to]=true;
+                path.add(e.to);
+                minDistance[e.to] = cur.w+e.w;
+                pq.offer(new Node(e.to, minDistance[e.to]));
+            }
+        }
     }
 
     private static class Node {
