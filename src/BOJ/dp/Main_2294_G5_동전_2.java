8de1cb0897b5340f1eebd35beda4ec91b938fab8
jhsseonn <hs125bt@gmail.com>
2024-09-12 23:44:22 +0900

    240912 BOJ 동전2

--- src/BOJ/dp/Main_2294_G5_동전_2.java
+++ src/BOJ/dp/Main_2294_G5_동전_2.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 14,224kb / 112ms
+ */
 public class Main_2294_G5_동전_2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
@@ -15,26 +18,27 @@ public class Main_2294_G5_동전_2 {
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] num = new int[n];
-        int[] dp = new int[k];
-        int cnt = 0;
+        int[] num = new int[n + 1];
+        int[] dp = new int[k + 1];
 
         for (int i = 1; i <= n; i++) {
             num[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(num);
+        Arrays.fill(dp, 10001);
 
+        dp[0] = 0;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
-                if (j % n == 0) {
-                    dp[j] = dp[j - i] + 1;
-                }
+            for (int j = num[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - num[i]] + 1);
             }
         }
 
+        System.out.println(Arrays.toString(dp));
+        if (dp[k] == 10001) dp[k] = -1;
 
-        sb.append(cnt);
+        sb.append(dp[k]);
         System.out.println(sb);
     }
 }
