d180511c11676d2e4c3e81adc272322c548a3ff7
janghyoseon <hosu0125@naver.com>
2024-09-15 16:50:23 +0900

    240915 BOJ 평범한 배낭

--- src/BOJ/dp/Main_12865_G5_평범한_배낭.java
+++ src/BOJ/dp/Main_12865_G5_평범한_배낭.java
@@ -3,6 +3,7 @@ package BOJ.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_12865_G5_평범한_배낭 {
@@ -14,10 +15,29 @@ public class Main_12865_G5_평범한_배낭 {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[][] items = new int[N][2];
+        int[][] items = new int[N+1][2];
+        int[][] dp = new int[N+1][K+1];
 
         for (int i = 1; i <= N; i++) {
-
+            st = new StringTokenizer(br.readLine());
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
         }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                if (j<items[i][0]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-items[i][0]]+items[i][1]);
+                }
+            }
+        }
+
+        for (int i = 0; i <= N; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        System.out.println(dp[N][K]);
     }
 }
