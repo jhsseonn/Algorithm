5b952022355992ad3432a7ea9396e6119264c629
효선 <hs125bt@gmail.com>
2024-09-23 01:20:15 +0900

    [Silver I] Title: 1로 만들기 2, Time: 124 ms, Memory: 22944 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        boolean[] visited = new boolean[N+1];
+        int[] before = new int[N+1];
+
+        int[] dp = new int[N+1];
+        dp[1] = 0;
+        for (int i = 2; i <=N ; i++) {
+            dp[i] = dp[i-1]+1;
+            before[i] = i-1;
+            if (i%3==0 && dp[i]>dp[i/3]+1) {
+                dp[i] = dp[i/3]+1;
+                before[i] = i/3;
+            }
+            if (i%2==0 && dp[i]>dp[i/2]+1) {
+                dp[i] = dp[i/2]+1;
+                before[i] = i/2;
+            }
+        }
+
+        System.out.println(dp[N]);
+        while (N>0) {
+            sb.append(N).append(" ");
+            N = before[N];
+        }
+        System.out.println(sb);
+    }
+}
