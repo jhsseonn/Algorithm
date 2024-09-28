02201b07fe0a2eb867fe3a327e51ccee9ce16131
jhsseonn <hs125bt@gmail.com>
2024-09-28 19:24:28 +0900

    240928 BOJ 타일 채우기 2

--- src/BOJ/dp/Main.java
+++ src/BOJ/dp/Main.java
@@ -3,7 +3,6 @@ package BOJ.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /**
  * 14,196kb / 104ms
@@ -18,27 +17,27 @@ public class Main {
         dp = new long[2][2];
         MOD = 1_000_000_007;
 
-        if (N%2!=0) {
+        if (N % 2 != 0) {
             System.out.println(0);
         } else {
-            if(N==2) System.out.println(3);
+            if (N == 2) System.out.println(3);
             else {
                 long[] base = {3, 1};
-                System.out.println(matrixMulti1(getBase(N/2), base)[1]);
+                System.out.println(matrixMulti1(getBase(N / 2), base)[1]);
             }
         }
     }
 
     private static long[][] getBase(long n) {
-        if (n==1) {
+        if (n == 1) {
             dp[0][0] = 4;
             dp[0][1] = -1;
             dp[1][0] = 1;
             dp[1][1] = 0;
             return dp;
         } else {
-            long[][] temp = getBase(n/2);
-            if (n%2==0) {
+            long[][] temp = getBase(n / 2);
+            if (n % 2 == 0) {
                 return matrixMulti2(temp, temp);
             } else {
                 return matrixMulti2(matrixMulti2(temp, temp), getBase(1));
@@ -49,18 +48,18 @@ public class Main {
     // 계수 패턴 행렬곱
     private static long[][] matrixMulti2(long[][] a, long[][] b) {
         long[][] result = new long[2][2];
-        result[0][0] = (a[0][0] * b[0][0] % MOD + a[0][1] * b[1][0] % MOD+MOD) % MOD;
-        result[0][1] = (a[0][0] * b[0][1] % MOD + a[0][1] * b[1][1] % MOD+MOD) % MOD;
-        result[1][0] = (a[1][0] * b[0][0] % MOD + a[1][1] * b[1][0] % MOD+MOD) % MOD;
-        result[1][1] = (a[1][0] * b[0][1] % MOD + a[1][1] * b[1][1] % MOD+MOD) % MOD;
+        result[0][0] = (a[0][0] * b[0][0] % MOD + a[0][1] * b[1][0] % MOD + MOD) % MOD;
+        result[0][1] = (a[0][0] * b[0][1] % MOD + a[0][1] * b[1][1] % MOD + MOD) % MOD;
+        result[1][0] = (a[1][0] * b[0][0] % MOD + a[1][1] * b[1][0] % MOD + MOD) % MOD;
+        result[1][1] = (a[1][0] * b[0][1] % MOD + a[1][1] * b[1][1] % MOD + MOD) % MOD;
         return result;
     }
 
     // base와의 행렬곱으로 정답 구하기
     private static long[] matrixMulti1(long[][] a, long[] b) {
         long[] result = new long[2];
-        result[0] = (a[0][0]*b[0]%MOD + a[0][1]*b[1]%MOD+MOD)%MOD;
-        result[1] = (a[1][0]*b[0]%MOD + a[1][1]*b[1]%MOD+MOD)%MOD;
+        result[0] = (a[0][0] * b[0] % MOD + a[0][1] * b[1] % MOD + MOD) % MOD;
+        result[1] = (a[1][0] * b[0] % MOD + a[1][1] * b[1] % MOD + MOD) % MOD;
         return result;
     }
 }
