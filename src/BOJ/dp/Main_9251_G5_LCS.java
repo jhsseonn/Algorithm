c13c9a9c66cfab05ef9766aedff85f874abcdda7
jhsseonn <hs125bt@gmail.com>
2024-10-10 00:02:06 +0900

    241009 BOJ LCS

--- src/BOJ/dp/Main_9251_G5_LCS.java
+++ src/BOJ/dp/Main_9251_G5_LCS.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- *
+ * 19,288kb / 132ms
  */
 public class Main_9251_G5_LCS {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
