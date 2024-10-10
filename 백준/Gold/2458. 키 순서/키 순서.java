1322ceed6a98f11fc43d6149e8474cc918e927c8
효선 <hs125bt@gmail.com>
2024-10-10 10:09:56 +0900

    [Gold IV] Title: 키 순서, Time: 620 ms, Memory: 42120 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M, cnt, cntReverse;
+    static List<Integer>[] nodes;
+    static List<Integer>[] nodesReverse;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nodes = new ArrayList[N + 1];
+        nodesReverse = new ArrayList[N + 1];
+        int ans = 0;
+
+        for (int i = 0; i < N + 1; i++) {
+            nodes[i] = new ArrayList<>();
+            nodesReverse[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            nodes[from].add(to);
+            nodesReverse[to].add(from);
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            cnt = 0;
+            cntReverse = 0;
+            dfs(i, new boolean[N+1]);
+            dfsReverse(i, new boolean[N+1]);
+            if (cnt + cntReverse-1 == N) ans += 1;
+        }
+
+        System.out.println(ans);
+    }
+
+    private static void dfs(int cur, boolean[] visited) {
+        visited[cur] = true;
+        cnt++;
+
+        for (int next : nodes[cur]) {
+            if (visited[next]) continue;
+            visited[next] = true;
+            dfs(next, visited);
+        }
+    }
+
+    private static void dfsReverse(int cur, boolean[] visited) {
+        visited[cur] = true;
+        cntReverse++;
+
+        for (int next : nodesReverse[cur]) {
+            if (visited[next]) continue;
+            visited[next] = true;
+            dfsReverse(next, visited);
+        }
+    }
+}
