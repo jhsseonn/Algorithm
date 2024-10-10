6d16137afa93e210704014cd1fb25ce176d53e61
jhsseonn <hosu0125@naver.com>
2024-10-10 10:00:40 +0900

    241010 BOJ 플로이드

--- src/BOJ/dp/Main_11404_G4_플로이드.java
+++ src/BOJ/dp/Main_11404_G4_플로이드.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- *
+ * 43,900kb / 384ms
  */
 public class Main_11404_G4_플로이드 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
