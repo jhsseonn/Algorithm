547383b681f0ae59c34dd418e56b27251b40b236
jhsseonn <hs125bt@gmail.com>
2024-10-11 23:48:42 +0900

    241011 SWEA 활주로 건설

--- src/swea/모의SW/Solution_4014_모의_활주로_건설.java
+++ src/swea/모의SW/Solution_4014_모의_활주로_건설.java
@@ -3,83 +3,77 @@ package swea.모의SW;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 19,036kb / 132ms
+ */
 public class Solution_4014_모의_활주로_건설 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static int N, X, result;
-    static int[][] arrOrigin, arrReverse;
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
 
-        for (int test_case = 1; test_case < T+1; test_case++) {
+        for (int test_case = 1; test_case < T + 1; test_case++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             X = Integer.parseInt(st.nextToken());
             result = 0;
-            arrOrigin = new int[N][N];
-            arrReverse = new int[N][N];
+            arr = new int[N][N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    arrOrigin[i][j] = Integer.parseInt(st.nextToken());
-                    arrReverse[j][i] = arrOrigin[i][j];
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            start(arrOrigin);
-            start(arrReverse);
+            for (int i = 0; i < N; i++) {
+                if (isValid(arr[i])) result++;
+                if (isValid(getColumn(i))) result++;
+            }
 
             sb.append("#").append(test_case).append(" ").append(result).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static void start(int[][] arr) {
-        for (int i = 0; i < N; i++) {
-            boolean isValid = false;  // 해당 행의 활주로 건설 가능 여부
-            int sameHeight = 1;
+    private static boolean isValid(int[] line) {
+        boolean[] visited = new boolean[N];
+        for (int i = 0; i < N-1; i++) {
+            if (line[i]==line[i+1]) continue;
 
-            // 경사로 설치
-            for (int j = 1; j < N; j++) {
-                // 이전 칸과 지금 칸이 다른 경우
-                if (arr[i][j] != arr[i][j - 1]) {
-                    if (arr[i][j-1]-arr[i][j]==1) {  // 내려가는 경사로 만들기
-                        sameHeight = 0;
-                        if (isValid(arr, i, j, arr[i][j])) i+=X-1;
-                        else {
-                            isValid = false;
-                            break;
-                        }
-                    } else if (arr[i][j]-arr[i][j-1]==1) {  // 올라가는 경사로 만들기
-                        if (sameHeight<X) {
-                            isValid = false;
-                            sameHeight = 1;
-                            break;
-                        }
-                        sameHeight = 1;
-                    } else {  // 경사로를 만들 수 없음
-                        isValid = false;
-                        sameHeight = 1;
-                        break;
-                    }
-                } else {
-                    sameHeight++;
+            if (Math.abs(line[i]-line[i+1])>1) return false;
+
+            if (line[i]-line[i+1]==1) {   // 내리막
+                for (int j = 1; j <= X; j++) {
+                    if (i+j >= N) return false;
+                    if (line[i+j]!=line[i+1]) return false;
+                    if (visited[i+j]) return false;
+                    visited[i+j] = true;
+                }
+            } else if (line[i+1]-line[i]==1) {  // 오르막
+                for (int j = 0; j < X; j++) {
+                    if (i-j < 0) return false;
+                    if (line[i-j]!=line[i]) return false;
+                    if (visited[i-j]) return false;
+                    visited[i-j] = true;
                 }
             }
-            if (isValid) result++;
         }
+        return true;
     }
 
-    private static boolean isValid(int[][] arr, int x, int y, int height) {
-        int k = 1;
-        while(y+k<N && arr[x][y+k]==height && k<X) k++;
-        if (k==X) return true;
-        else return false;
+    private static int[] getColumn(int col) {
+        int[] column = new int[N];
+        for (int i = 0; i < N; i++) {
+            column[i] = arr[i][col];
+        }
+
+        return column;
     }
 }
