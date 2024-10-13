c578cefcc35311e0eded19bf863326b0bb047c6d
jhsseonn <hs125bt@gmail.com>
2024-10-13 18:19:40 +0900

    241013 BOJ Cubeditor

---
+++ src/BOJ/string/Main_1701_G2_Cubeditor.java
@@ -0,0 +1,47 @@
+package BOJ.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 77,580kb / 312ms
+ */
+public class Main_1701_G2_Cubeditor {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        str = br.readLine();
+        int maxLen = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            String suffix = str.substring(i);
+            int[] pi = getPI(suffix);
+
+            for (int pattern:pi) {
+                maxLen = Math.max(maxLen, pattern);
+            }
+        }
+
+        System.out.println(maxLen);
+    }
+
+    private static int[] getPI(String pattern) {
+        int n = pattern.length();
+        int[] pi = new int[n];
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = pi[j - 1];
+            }
+
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                pi[i] = ++j;
+            }
+        }
+
+        return pi;
+    }
+}
