4aebdaa20eec1d62edee28d71b6b8cc6c9b3a2eb
jhsseonn <hs125bt@gmail.com>
2024-10-15 23:36:25 +0900

    241015 BOJ 보석도둑

--- src/BOJ/priorityQueue/Main_1202_G2_보석도둑.java
+++ src/BOJ/priorityQueue/Main_1202_G2_보석도둑.java
@@ -7,24 +7,26 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/**
+ * 133,708kb / 1,840ms
+ */
 public class Main_1202_G2_보석도둑 {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static PriorityQueue<Jewel> jewels = new PriorityQueue<>();
-	static int[] bags;
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		bags = new int[K];
-		int result = 0;
+		int[] bags = new int[K];
+		long result = 0;
+		Jewel[] jewels = new Jewel[N];
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			jewels.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+			jewels[i] = new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -32,16 +34,17 @@ public class Main_1202_G2_보석도둑 {
 		}
 		
 		Arrays.sort(bags);
+		Arrays.sort(jewels);
 		
 		// 가방 돌면서 최고 제한 무게보다 작거나 같은 경우 가격이 가장 비싼 경우를 합한다
+		int idx = 0;
 		for (int i = 0; i < K; i++) {
-			for (int j = 0; j < jewels.size(); j++) {
-				Jewel jl = jewels.peek();
-				if (bags[i]>=jl.m) {
-					result+=jl.v;
-					jewels.poll();
-					break;
-				}
+			while(idx<N && bags[i]>=jewels[idx].m) {
+				pq.offer(jewels[idx].v);
+				idx++;
+			}
+			if (!pq.isEmpty()) {
+				result += pq.poll();
 			}
 		}
 		
@@ -59,7 +62,7 @@ public class Main_1202_G2_보석도둑 {
 
 		@Override
 		public int compareTo(Jewel o) {
-			return this.m!=o.m ? this.m-o.m : this.v-o.v;
+			return this.m!=o.m ? Integer.compare(this.m, o.m) : Integer.compare(o.v, this.v);
 		}
 	}
 
