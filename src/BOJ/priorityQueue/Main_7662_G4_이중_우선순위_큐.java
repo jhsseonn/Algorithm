4004d74e8babff5ec8a06d9d7410f35b3c5c83f6
jhsseonn <hs125bt@gmail.com>
2024-10-15 23:03:03 +0900

    241015 BOJ 이중 우선순위 큐

--- src/BOJ/priorityQueue/Main_7662_G4_이중_우선순위_큐.java
+++ src/BOJ/priorityQueue/Main_7662_G4_이중_우선순위_큐.java
@@ -3,65 +3,60 @@ package com.ssafy.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayDeque;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
+/**
+ * 358,168kb / 2264ms
+ */
 public class Main_7662_G4_이중_우선순위_큐 {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
-	static PriorityQueue<Integer> qMin;
-	static PriorityQueue<Integer> qMax;
 
 	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
-		qMin = new PriorityQueue<>(new Comparator<Integer>() {
 
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o1-o2;
-			}
-		});
-		qMax = new PriorityQueue<>(new Comparator<Integer>() {
-
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o2-o1;
-			}
-		});
-
-		for (int i = 0; i < T; i++) {
+		for (int i = 1; i <= T; i++) {
 			int K = Integer.parseInt(br.readLine());
+			TreeMap<Integer, Integer> tm = new TreeMap<>();
+
 			for (int j = 0; j < K; j++) {
 				st = new StringTokenizer(br.readLine());
 				char cal = st.nextToken().charAt(0);
 				int N = Integer.parseInt(st.nextToken());
 
 				if (cal=='I') {
-					qMin.offer(N);
-					qMax.offer(N);
+					tm.put(N, tm.getOrDefault(N, 0)+1);
 				} else if (cal=='D') {
-					if (!qMin.isEmpty() && !qMax.isEmpty()) {
-						if (N == -1) {
-							int r = qMin.poll();
-							qMax.remove(r);
-						} else if (N == 1) {
-							int r = qMax.poll();
-							qMin.remove(r);
-						}
+					if (tm.isEmpty()) continue;
+					if (N == -1) {
+						int first = tm.firstKey();
+						int value = tm.getOrDefault(first, 0);
+						if (value==1 || value==0) tm.remove(first);
+						else tm.put(first, value-1);
+					} else if (N == 1) {
+						int last = tm.lastKey();
+						int value = tm.getOrDefault(last, 0);
+						if (value==1 || value==0) tm.remove(last);
+						else tm.put(last, value-1);
 					}
 				}
 			}
-			if (qMin.isEmpty() && qMax.isEmpty()) {
+			if (tm.isEmpty()) {
 				sb.append("EMPTY").append("\n");
 				continue;
 			}
-			sb.append(qMax.poll()+" "+qMin.poll()).append("\n");
+
+			sb.append(tm.lastKey()).append(" ").append(tm.firstKey()).append("\n");
 		}
-		
+
 		System.out.println(sb);
 	}
-
 }
