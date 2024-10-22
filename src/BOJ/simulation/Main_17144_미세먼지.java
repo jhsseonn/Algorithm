51df0660b83d519521337199108d9e7f79972f2c
jhsseonn <hs125bt@gmail.com>
2024-10-23 02:12:20 +0900

    241022 미세먼지 풀이중

--- src/BOJ/simulation/Main_17144_미세먼지.java
+++ src/BOJ/simulation/Main_17144_미세먼지.java
@@ -4,16 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import javax.swing.plaf.DesktopIconUI;
 
 // 17144 미세먼지 안녕!
 public class Main_17144_미세먼지 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int R, C;
+    static int[][] circulator;
     static Dust[][] room;
-    static List<Pos> circulator;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int[] dr = {0, -1, 0, 1};
+    static int[] dc = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         // 첫째줄 입력
@@ -22,14 +23,17 @@ public class Main_17144_미세먼지 {
         C = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
         room = new Dust[R][C];
-        circulator = new ArrayList<>();
+        circulator = new int[2][2];
 
+        int cidx = 0;
         // 방 내부 입력(공기청정기, 미세먼지)
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < C; j++) {
                 room[i][j] = new Dust(Integer.parseInt(st.nextToken()), 0);
-                if (room[i][j].origin==-1) circulator.add(new Pos(i, j));
+                if (room[i][j].origin==-1) {
+                    circulator[cidx++] = new int[] {i, j};
+                }
             }
         }
 
@@ -53,10 +57,16 @@ public class Main_17144_미세먼지 {
 
     // 미세먼지 확산
     private static void microDust() {
+        Dust[][] temp = new Dust[R][C];
+        for (int i = 0; i < R; i++) {
+            temp[i] = room[i].clone();
+        }
+
         for (int r = 0; r < R; r++) {
             for (int c = 0; c < C; c++) {
                 // 확산되는 양: 5로 나눈 몫
                 int origin = room[r][c].origin;
+                if (origin<5) continue;
                 int val = origin/5;
                 int cnt = 0;
 
@@ -66,108 +76,83 @@ public class Main_17144_미세먼지 {
                     int nc = c+dc[i];
 
                     // 공기청정기가 있거나 범위 밖이면 확산x
-                    if (nr<0 || nr>=R || nc<0 || nc>=C) continue;
-                    int dust = room[nr][nc].origin;
-                    if (dust==-1) continue;
+                    if (nr<0 || nr>=R || nc<0 || nc>=C || room[nr][nc].origin==-1) continue;
 
-                    room[nr][nc].sum += val;
-                    cnt+=1;
+                    temp[nr][nc].sum += val;
+                    cnt++;
                 }
 
-                // 남은 양: A[r][c]-(A[r][c]/5*cnt)
-                room[r][c].origin = origin-(val*cnt);
+                temp[r][c].origin -= val*cnt;
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                temp[i][j].updateOriginalValue();
             }
         }
 
         // 각 칸의 먼지양 갱신
-        for (int r = 0; r < R; r++) {
-            for (int c = 0; c < C; c++) {
-                room[r][c].updateOriginValue();
-            }
-        }
+        room = temp;
     }
 
     // 공기청정기 바람 순환
     private static void circulate() {
-        // 반시계 방향
-        int[] udr = {1, 0, -1, 0};
-        int[] udc = {0, -1, 0, 1};
+        Dust[][] temp = new Dust[R][C];
+        for (int i = 0; i < R; i++) {
+            temp[i] = room[i].clone();
+        }
 
         // 위쪽 순환 - 반시계 방향으로 밀기
-        Queue<Pos> q = new ArrayDeque<>();
-        Pos up = circulator.get(0);
-        q.offer(up);
         int idx = 0;
-        while(!q.isEmpty()) {
-            Pos cur = q.poll();
-            int nr = cur.r+udr[idx];
-            int nc = cur.c+udc[idx];
-            if (nr<0 || nr>circulator.get(0).r || nc<0 || nc>=C ) {
+        int ur = circulator[0][0];
+        int uc = circulator[0][1];
+        while(true) {
+            int nr = ur+dr[idx];
+            int nc = uc+dc[idx];
+            if (nr<0 || nr>circulator[0][0] || nc<0 || nc>=C) {
                 idx = (idx+1)%4;
                 continue;
             }
-            if (room[nr][nc].origin==-1) {
-                break;
-            }
-            q.offer(new Pos(nr, nc));
-
-            if (room[cur.r][cur.c].origin==-1) {  // 이전 칸이 공기청정기인 경우
-                room[nr][nc].origin = 0;  // 먼지 없애고
-                continue;
-            }
-            room[nr][nc].origin = room[cur.r][cur.c].origin;  // 이전 칸의 먼지를 지금 칸에 넣어줌
+            if (nr==circulator[0][0] && nc==circulator[0][1]) break;
+            temp[nr][nc] = room[ur][uc];
+            ur = nr;
+            uc = nc;
         }
-
-        // 시계 방향
-        int[] ddr = {1, 0, -1, 0};
-        int[] ddc = {0, 1, 0, -1};
+        temp[ur][uc].origin=-1;
 
         // 아래쪽 순환 - 시계 방향으로 밀기
-        Queue<Pos> q2 = new ArrayDeque<>();
-        Pos down = circulator.get(1);
-        q2.offer(down);
-        int idx2 = 0;
-        while(!q2.isEmpty()) {
-            Pos cur = q2.poll();
-            int nr = cur.r+ddr[idx2];
-            int nc = cur.c+ddc[idx2];
-            if (nr<circulator.get(1).r || nr>=R || nc<0 || nc>=C) {
-                idx2 = (idx2+1)%4;
+        int dor = circulator[1][0];
+        int doc = circulator[1][1];
+        idx = 0;
+        while(true) {
+            int nr = dor+dr[(idx+2)%4];
+            int nc = doc+dc[(idx+2)%4];
+            if (nr<circulator[1][0]|| nr>=R || nc<0 || nc>=C) {
+                idx = (idx+1)%4;
                 continue;
             }
-            if (room[nr][nc].origin==-1) {
-                break;
-            }
-            q2.offer(new Pos(nr, nc));
-
-            if (room[cur.r][cur.c].origin==-1) {  // 이전 칸이 공기청정기인 경우
-                room[nr][nc].origin = 0;  // 먼지 없애고
-                continue;
-            }
-            room[nr][nc].origin = room[cur.r][cur.c].origin;  // 이전 칸의 먼지를 지금 칸에 넣어줌
+            if (nr==circulator[1][0] && nc==circulator[1][1]) break;
+            temp[nr][nc] = room[dor][doc];
+            dor = nr;
+            doc = nc;
         }
-    }
+        temp[dor][doc].origin = -1;
 
-    static class Pos {
-        int r, c;
-
-        public Pos(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
+        room = temp;
     }
 
     static class Dust {
         int origin;
         int sum;
 
-        public Dust(int origin, int sum) {
+        public Dust(final int origin, final int sum) {
             this.origin = origin;
             this.sum = sum;
         }
 
-        public void updateOriginValue() {
-            this.origin += this.sum;
+        public void updateOriginalValue() {
+            this.origin+=this.sum;
             this.sum = 0;
         }
     }
