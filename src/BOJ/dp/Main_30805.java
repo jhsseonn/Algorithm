b9d0b440bef04fb40dfd0bb04fd5c122cb8acd41
hosu0125 <hosu0125@naver.com>
2024-10-29 18:00:48 +0900

    241028 BOJ 사전 순 최대 공통 부분 수열 풀이중

--- src/BOJ/dp/Main_30805.java
+++ src/BOJ/dp/Main_30805.java
@@ -4,16 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-// 정렬, 그리디
 public class Main_30805 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
     static int[] A, B;
-    static List<Integer> commonNums;
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
@@ -30,19 +28,73 @@ public class Main_30805 {
             B[i] = Integer.parseInt(st.nextToken());
         }
 
-        commonNums = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (A[i]==B[j]) {
-                    commonNums.add(A[i]);  // 공통인 숫자 저장
+        // 공통 부분 수열 구하기
+        List<Integer> commonNums = new ArrayList<>();
+        int aIdx = 0, bIdx = 0;
+        while (aIdx<N && bIdx<M) {
+            if (A[aIdx]==B[bIdx]) {
+                commonNums.add(A[aIdx]);
+                aIdx++;
+                bIdx++;
+            } else {
+                int sameAIdx = aIdx, sameBIdx = bIdx;
+                while (sameBIdx < M && A[aIdx]!=B[sameBIdx]) {
+                    sameBIdx++;
+                }
+                while (sameAIdx < N && A[sameAIdx]!=B[bIdx]) {
+                    sameAIdx++;
+                }
+
+                if (sameAIdx<=sameBIdx) {
+                    aIdx=sameAIdx;
+                    if (aIdx<N) {
+                        commonNums.add(A[aIdx]);
+                    }
+                    aIdx++;
+                } else {
+                    bIdx=sameBIdx;
+                    if (bIdx<M) {
+                        commonNums.add(B[bIdx]);
+                    }
+                    bIdx++;
                 }
             }
         }
 
+        List<Number> nums = new ArrayList<>();
+        for (int i = 0; i < commonNums.size(); i++) {
+            nums.add(new Number(i, commonNums.get(i)));
+        }
 
+        Collections.sort(nums);
+
+        int idx = 0;
+        List<Number> res = new ArrayList<>();
+        for (Number n : nums) {
+            if (idx <= n.index) {
+                idx = n.index;
+                res.add(n);
+            }
+        }
+
+        System.out.println(res.size());
+        for (int i = 0; i < res.size(); i++) {
+            System.out.print(res.get(i).value + " ");
+        }
     }
 
-    private void subset() {
+    private static class Number implements Comparable<Number> {
+        int index, value;
 
+        public Number(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Number o) {
+            return this.value != o.value ? o.value - this.value : this.index - o.index;
+        }
     }
+
 }
