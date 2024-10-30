fc8a8c425aca46376dbaa59d6bd583631d04a859
효선 <hs125bt@gmail.com>
2024-10-31 08:43:32 +0900

    [Gold IV] Title: 사이클 게임, Time: 648 ms, Memory: 244676 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] parents;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Edge[] edges = new Edge[M];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(from, to);
+        }
+
+        make();
+
+        int cnt = 0;
+        int ans = 0;
+        for (Edge now : edges) {
+            if (union(now.from, now.to)) {
+                cnt++;
+            } else {
+                ans = ++cnt;
+                break;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static void make() {
+        parents = new int[N + 1];
+        Arrays.fill(parents, -1);
+    }
+
+    private static int findSet(int a) {
+        if (parents[a] < 0) {
+            return a;
+        } else {
+            return parents[a] = findSet(parents[a]);
+        }
+    }
+
+    private static boolean union(int a, int b) {
+        int parentA = findSet(a);
+        int parentB = findSet(b);
+        if (parentA == parentB) {
+            return false;
+        }
+
+//        parents[parentB] += parents[parentA];
+        parents[parentA] = parentB;
+        return true;
+    }
+
+    private static class Edge {
+        int from, to;
+
+        public Edge(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
