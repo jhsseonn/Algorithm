87fb813988e0309c860188d584b4cb9bebde1479
hosu0125 <hosu0125@naver.com>
2024-11-08 17:37:03 +0900

    241108 BOJ 사회망서비스

--- src/BOJ/graph/Main_2533_사회망서비스.java
+++ src/BOJ/graph/Main_2533_사회망서비스.java
@@ -3,23 +3,53 @@ package BOJ.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * 428,740kb / 2,152ms
+ */
 public class Main_2533_사회망서비스 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+    static List<Integer>[] adjList;
+    static int[][] dp;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N][N];
+        dp = new int[N+1][2];
+        adjList = new ArrayList[N+1];
+        visited = new boolean[N+1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N-1; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
 
+            adjList[from].add(to);
+            adjList[to].add(from);
+        }
 
+        dfs(1);
+        System.out.println(Math.min(dp[1][0], dp[1][1]));
+    }
+
+    private static void dfs(int depth) {
+        visited[depth] = true;
+        dp[depth][0] = 0;
+        dp[depth][1] = 1;  // 해당 지점도 얼리어답터이므로 1로 초기화
+
+        for (Integer child:adjList[depth]) {
+            if (visited[child]) continue;
+            dfs(child);
+            dp[depth][0] += dp[child][1];
+            dp[depth][1] += Math.min(dp[child][0], dp[child][1]);
         }
     }
 }
