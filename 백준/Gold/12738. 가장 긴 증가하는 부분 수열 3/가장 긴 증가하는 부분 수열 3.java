e2be571ae20e2097858f0452ac54ae7260ec7032
효선 <hs125bt@gmail.com>
2024-12-29 20:42:17 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 612 ms, Memory: 133200 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int[] arr;
+    static List<Integer> lis = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int num:arr) {
+            int pos = binarySearch(num);
+            if (pos==lis.size()) lis.add(num);
+            else lis.set(pos, num);
+        }
+
+        System.out.println(lis.size());
+    }
+
+    private static int binarySearch(int key) {
+        int from = 0, end = lis.size()-1;
+
+        while(from<=end) {
+            int mid = from+(end-from)/2;
+            if (lis.get(mid)<key) from = mid+1;
+            else end = mid-1;
+        }
+
+        return from;
+    }
+}
