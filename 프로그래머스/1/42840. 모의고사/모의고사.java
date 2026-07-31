f44379320aba34b4e112eecc9ba8e3e14b38578c
효선 <hs125bt@gmail.com>
2026-07-31 23:26:07 +0900

    [level 1] Title: 모의고사, Time: 6.23 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,20 @@
-import java.util.*;
-
+import java.util.ArrayList;
 class Solution {
-    
-    public int[] solution(int[] answers) {
-        int[] answer = {};
-        
-        int arrayLen = answers.length;
-        
-        int[] pattern1 = {1, 2, 3, 4, 5};
-        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
+    public int[] solution(int[] answer) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int[] score = new int[3];
-        
-        for (int i=0; i<arrayLen; i++) {
-            if (pattern1[i%5]==answers[i]) score[0]++;
-            if (pattern2[i%8]==answers[i]) score[1]++;
-            if (pattern3[i%10]==answers[i]) score[2]++;
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
         }
-        
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        List<Integer> temp = new ArrayList<>();
-        
-        for (int i = 0; i<3; i++) {
-            if (score[i]==max) temp.add(i+1);
-        }
-        
-        answer = new int[temp.size()];
-        for (int i=0; i<temp.size(); i++) {
-            answer[i] = temp.get(i);
-        }
-        
-        return answer;
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> list = new ArrayList<>();
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
